17444a24499b253fdae9075d91c71c4f4b8ecb35
Angela Kock <angela@jhb.dvt.co.za>
2019-07-10 12:22:35 +0200

    Move all the phrases to arrays in ts file, in order to create for loops

--- src/app/phrases-keyboard/phrases-keyboard.component.ts
+++ src/app/phrases-keyboard/phrases-keyboard.component.ts
@@ -17,9 +17,60 @@ export class PhrasesKeyboardComponent implements OnInit {
   trustedUrl: any;
   speech = new Speech();
 
+  verbs = [];
+  adjectives = [];
+  pronouns = [];
+  conjunctions = [];
+  prepositions = [];
+
   constructor() {
     this.speech.setLanguage('en-US');
     this.speech.setVolume(1);
+
+    this.verbs = [
+      'can', 'do', 'come', 'draw', 'drink',
+      'eat', 'get', 'go', 'keep', 'know',
+      'read', 'sleep', 'speak', 'stand',
+      'write', 'shut', 'show', 'send',
+      'think', 'sing', 'put',
+      'pay', 'lie', 'let', 'hide', 'hear',
+      'give', 'choose', 'buy', 'catch'
+    ];
+
+    this.adjectives = [
+      'soft', 'quiet', 'loud', 'whisper', 'little',
+      'tall', 'thin', 'long', 'late', 'slow',
+      'old', 'young', 'quick', 'heavy',
+      'light', 'very', 'full', 'few',
+      'lucky', 'fine', 'fantastic', 'funny',
+      'helpful', 'careful', 'simple', 'hard', 'high',
+    ];
+
+    this.pronouns = [
+      'I', 'you', 'he', 'she', 'it',
+      'they', 'me', 'you', 'him', 'her',
+      'my', 'mine', 'your', 'yours',
+      'his', 'her', 'who', 'whom',
+      'whose', 'that', 'which', 'this',
+      'myself', 'herself', 'itself', 'yourself'
+    ];
+
+    this.conjunctions = [
+      'after', 'although', 'as', 'is', 'because',
+      'before', 'even', 'once', 'since', 'than',
+      'though', 'until', 'when',
+      'where', 'while', 'both',
+      'either', 'neither', 'but', 'yet',
+      'or', 'so', 'and'
+    ];
+
+    this.prepositions = [
+      'at', 'from', 'into', 'during', 'including',
+      'against', 'during', 'by', 'about', 'through',
+      'after', 'between', 'since', 'without',
+      'along', 'following', 'across', 'except',
+      'down', 'around', 'in', 'on'
+    ];
   }
 
   ngOnInit() { }
@@ -32,7 +83,8 @@ export class PhrasesKeyboardComponent implements OnInit {
     this.numberArray.push(button);
   }
 
-  getPhrases() {
+  getPhrases($phrase) {
+
     const phrase = this.numberArray.toString();
 
     this.speech.speak({
@@ -50,6 +102,4 @@ export class PhrasesKeyboardComponent implements OnInit {
     this.phrases = null;
     this.numberArray.pop();
   }
-
-
 }
